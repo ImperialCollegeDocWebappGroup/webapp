f1d7f76e08648b2dbfcfdb28bcfc08f4bc231f47
TimelessJY <samantha-shan@Hotmail.com>
2015-06-01 13:32:37 +0100

    Added custom URL scheme. Import github project HTML parser. Able to find the correct "div class" tag, but can't load the following information yet.

--- webapp/AppDelegate.swift
+++ webapp/AppDelegate.swift
@@ -42,4 +42,66 @@ import UIKit
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        // when the app is launched in a website
+        //  NSLog("This is the URL: %@",url)
+        
+        let realURL = url.absoluteString!.stringByReplacingOccurrencesOfString("myfittingroom://", withString: "http://")
+        
+        println("real URL is : \(realURL)")
+        
+        if let myURL = NSURL(string: realURL) {
+            var error: NSError?
+            let myHTMLString = NSString(contentsOfURL: myURL, encoding: NSUTF8StringEncoding, error: &error)
+            
+            if let error = error {
+                println("Error : \(error)")
+            } else {
+               // println("HTML : \(myHTMLString)")
+                println("success")
+           //     println("HTML:\(myHTMLString as! String)")
+                /*
+                
+                let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"*/
+                
+                var err : NSError?
+                var parser     = HTMLParser(html: (myHTMLString as! String), error: &err)
+                if err != nil {
+                    println(err)
+                    exit(1)
+                }
+                
+                var bodyNode   = parser.body
+           /*
+                if let inputNodes = bodyNode?.findChildTags("div") {
+                    for node in inputNodes {
+                        println(node.contents)
+                    }
+                }
+          */
+                if let inputNodes = bodyNode?.findChildTags("div") {
+                    for node in inputNodes {
+                  /*      println(node.contents)
+                        println(node.getAttributeNamed("class"))
+    */
+                        if node.getAttributeNamed("class") == "productImage" {
+                            println("found!!!!!!!!")
+                            println(node.contents)
+
+                        }
+                    }
+                }
+
+            }
+        } else {
+            println("Error: \(realURL) doesn't seem to be a valid URL")
+        }
+        
+        
+        //  println(realURL)
+        return true
+        
+    }
+    
 }
